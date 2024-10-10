f9ff1ac389d3d4efa66922c101853666c1a09927
Nikita-Agarwal07 <nikita.agarwal0709@gmail.com>
2024-10-10 12:43:42 +0530

    Update carsousel.js

--- blocks/carsousel/carsousel.js
+++ blocks/carsousel/carsousel.js
@@ -11,17 +11,22 @@ export default function decorate(block) {
     if (r === 0) {
       const nextbtn = document.createElement('button');
       nextbtn.classList.add('btn', 'btn-next');
+
       const node = document.createTextNode(btnNxt);
       nextbtn.appendChild(node);
+
       row.replaceWith(nextbtn);
     } else if (r === rows.length - 1) {
       const prebtn = document.createElement('button');
       prebtn.classList.add('btn', 'btn-prev');
+
       const node = document.createTextNode(btnPre);
       prebtn.appendChild(node);
+
       row.replaceWith(prebtn);
     } else {
       row.classList.add('slide');
+
       Array.from(row.children).forEach((col, c) => {
         if (c === 1) {
           col.classList.add('slide-text');
@@ -31,6 +36,8 @@ export default function decorate(block) {
   });
 
   const slides = document.querySelectorAll('.slide');
+
+  // Ensure proper empty lines for CSS linting rules
 
   // Loop through slides and set each slide's translateX
   slides.forEach((slide, indx) => {
